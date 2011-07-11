9c41b935e11a9c60d1eaf8158458948778f93bc2
Chris Fordham <chris@xhost.com.au>
2011-07-11 14:50:13 +1000

    Initial commit of rs-config-api.sh.

---
+++ bin/rs-config-api.sh
@@ -0,0 +1,66 @@
+#!/bin/sh -e
+
+# rs-config-api.sh
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+# functions
+asksure() {
+	echo "Are you sure (Y/N)? "
+	while read -r -n 1 -s answer; do
+  		if [[ $answer = [YyNn] ]]; then
+    		[[ $answer = [Yy] ]] && retval=0
+    		[[ $answer = [Nn] ]] && retval=1
+    		break
+  		fi
+	done
+	return $retval
+}
+
+read -rp 'RightScale Username: ' rs_api_user
+	until [[ $rs_api_user ]]
+	do read -rp 'Requires a value, try again: ' rs_api_user
+done
+
+read -rp 'RightScale Password: ' rs_api_password
+	until [[ $rs_api_password ]]
+	do read -rp 'Requires a value, try again: ' rs_api_password
+done
+
+read -rp 'RightScale Account ID: ' rs_api_account_id
+	until [[ $rs_api_account_id ]]
+	do read -rp 'Requires a value, try again: ' rs_api_account_id
+done
+
+read -rp 'RightScale API Version (e.g. 1.0): ' rs_api_version
+	until [[ $rs_api_version ]]
+	do read -rp 'Requires a value, try again: ' rs_api_version
+done
+
+# confirmation
+echo
+echo '== Confirmation =='
+echo "User: $rs_api_user"
+echo "Password: $rs_api_password"
+echo "Account ID: $rs_api_account_id"
+echo "API Version: $rs_api_version"
+echo "=="
+echo
+
+# ask if ok to save
+echo 'Is this correct?'
+if asksure; then
+cat <<EOF> "$HOME/.rightscale/rs_api_config.sh"
+rs_api_version=$rs_api_version
+rs_api_cookie="$HOME/.rightscale/rs_api_cookie.txt"
+EOF
+cat <<EOF> "$HOME/.rightscale/rs_api_creds.sh"
+rs_api_account_id=$rs_api_account_id
+rs_api_user="$rs_api_user"
+rs_api_password="$rs_api_password"
+EOF
+	echo 'Settings saved.'
+else
+	echo "operation cancelled."
+fi
