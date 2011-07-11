3c146ae1e03dddce335b902d215cc78cfd915eb3
Chris Fordham <chris@xhost.com.au>
2011-07-12 08:32:26 +1000

    Initial add of rs-get-server-array.sh.

---
+++ bin/rs-get-server-array.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+# rs-get-server-array <server_array_id>
+
+[[ ! $1 ]] && echo 'No server array ID provided, exiting.' && exit 1
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+array_id="$1"
+
+url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$array_id"
+echo "GET: $url"
+api_result=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
+echo "$api_result"
