78d04783e32df6573694dd0a18cd07beb1ca899c
Chris Fordham <chris@xhost.com.au>
2011-07-12 08:25:57 +1000

    Initial add of rs-delete-deployment.sh.

---
+++ bin/rs-delete-deployment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+# rs-delete-deployment <deployment_id>
+
+[[ ! $1 ]] && echo 'No deployment ID provided, exiting.' && exit 1
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+deployment_id="$1"
+
+url="https://my.rightscale.com/api/acct/$rs_api_account_id/deployments/$deployment_id"
+echo "DELETE: $url"
+api_result=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" --request DELETE "$url")
+echo "$api_result"
