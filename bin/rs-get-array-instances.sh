632728bbb95c932f68815a60d484891bb894b870
Chris Fordham <chris@xhost.com.au>
2012-01-09 18:58:20 +1100

    Initial add of rs-get-array-instances.sh.

---
+++ bin/rs-get-array-instances.sh
@@ -0,0 +1,26 @@
+# !/bin/sh -e
+
+# rs-get-array-instances <server_array_id> [<api_version>]
+
+if [ ! $1 ]; then 
+	echo 'No server array ID provided, exiting.'
+	echo "Usage: rs-get-array-instances <server_array_id> [<api_version>]"
+	exit 1
+fi
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+array_id="$1"
+[ "$2" ] && rs_api_version="$2"
+
+if [ "$rs_api_version" = 1.5 ]; then
+	url="https://my.rightscale.com/api/server_arrays/$array_id/current_instances.xml"
+	rs_api_version=1.5
+else
+	url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$array_id"
+fi
+
+echo "GET: $url"
+api_result=$(curl -X GET -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
+echo "$api_result"
