ea9d1dc7102be6f7eca6b4a5bf8aa6fb5ea97af8
Chris Fordham <chris@xhost.com.au>
2012-03-10 05:48:38 +1100

    Fix quoting and mandate ServerTemplate ID.

--- bin/rs-run-rightscript-array.sh
+++ bin/rs-run-rightscript-array.sh
@@ -6,7 +6,7 @@
 
 [[ ! $1 ]] && echo 'No rightscript ID provided, exiting.' && exit 1
 [[ ! $2 ]] && echo 'No server array ID provided, exiting.' && exit 1
-#[[ ! $3 ]] && echo 'No servertemplate ID provided, exiting.' && exit 1
+[[ ! $3 ]] && echo 'No servertemplate ID provided, exiting.' && exit 1
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
@@ -14,11 +14,12 @@
 rightscript_id="$1"
 server_array_id="$2"
 server_template_id="$3"
-
 api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$server_array_id/run_script_on_all"
-api_params="-d server_array[right_script_href]=https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts/$rightscript_id"
-[[ $3 ]] && api_params="$api_params -d server_array[server_template_hrefs]=https://my.rightscale.com/api/acct/$rs_api_account_id/ec2_server_templates/$3"
 
 echo "Running RightScript [$rightscript_id] on Server Array [$server_array_id]."
 echo "GET: $api_url"
-curl -X POST -H X-API-VERSION:"$rs_api_version" -b "$rs_api_cookie" "$api_url" "$api_params"
+curl -H X-API-VERSION:"$rs_api_version" -X POST -g \
+-b "$rs_api_cookie" \
+-d "server_array[right_script_href]=https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts/$rightscript_id" \
+-d "server_array[server_template_hrefs]=https://my.rightscale.com/api/acct/$rs_api_account_id/ec2_server_templates/$3" \
+"$api_url"
