afaf16f527d019ca5d0fa0716c2a6d00088dd303
Chris Fordham <chris@xhost.com.au>
2012-06-18 12:28:23 +1000

    Modify to use rightscript_href correctly from right_script_id.

--- bin/rs-run-rightscript.sh
+++ bin/rs-run-rightscript.sh
@@ -5,16 +5,17 @@
 # e.g.   rs-run-rightscript-array.sh 1234 4321
 
 [[ ! $1 ]] && echo 'No server_id ID provided, exiting.' && exit 1
-[[ ! $2 ]] && echo 'No right_script_href ID provided, exiting.' && exit 1
+[[ ! $2 ]] && echo 'No right_script_id ID provided, exiting.' && exit 1
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
 server_id="$1"
-rightscript_href="$2"
+rightscript_id="$2"
+rightscript_href="https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts/$rightscript_id"
 
 api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers/$server_id/run_script"
 
 echo "Running RightScript [$rightscript_href] on Server [$server_id]."
 echo "GET: $api_url"
-curl -H X-API-VERSION:"$rs_api_version" -X POST -b "$rs_api_cookie" -d "server[right_script_href]=$rightscript_href" "$api_url"
+curl -s -S -v -X POST -b "$rs_api_cookie" -H X-API-VERSION:"$rs_api_version" -d server[right_script_href]="$rightscript_href" "$api_url"
