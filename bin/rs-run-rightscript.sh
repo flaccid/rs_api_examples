71a61c9ddba753a8d1589cad0a3e0bbe5a84fe31
Chris Fordham <chris.fordham@rightscale.com>
2012-06-22 18:23:59 +1000

    Add in support for args passing.

--- bin/rs-run-rightscript.sh
+++ bin/rs-run-rightscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-# rs-run-rightscript <server_id> <right_script_href>
+# rs-run-rightscript <server_id> <right_script_id> [[extra curl post params]]
 
-# e.g.   rs-run-rightscript-array.sh 1234 4321
+# e.g.   rs-run-rightscript-array.sh 1234 4321 "server[parameters][HELLO_WORLD_TEXT]=text:hello world" "server[ignore_lock]=true"
 
 [[ ! $1 ]] && echo 'No server_id ID provided, exiting.' && exit 1
 [[ ! $2 ]] && echo 'No right_script_id ID provided, exiting.' && exit 1
@@ -12,14 +12,19 @@
 
 server_id="$1"
 rightscript_id="$2"
-rightscript_href="https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts/$rightscript_id"
+args=()
+shift; shift
+for arg in "$@"; do
+	args+=(-d "$arg")
+done
 
+rightscript_href="https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts/$rightscript_id"
 api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers/$server_id/run_script"
 
 echo "Running RightScript [$rightscript_href] on Server [$server_id]."
 echo "GET: $api_url"
 
-api_result=$(curl -s -S -i -X POST -b "$rs_api_cookie" -H X-API-VERSION:"$rs_api_version" -d server[right_script_href]="$rightscript_href" "$api_url")
+api_result=$(curl -s -S -i -X POST -b "$rs_api_cookie" -H X-API-VERSION:"$rs_api_version" -d server[right_script_href]="$rightscript_href" "${args[@]}" "$api_url")
 
 case "$api_result" in 
   *Status:\ 201*)
