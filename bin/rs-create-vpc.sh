53ae459b5a5c80993e098b32e9d8cdb9690f8142
Chris Fordham <chris.fordham@rightscale.com>
2012-08-22 18:41:12 +1000

    Add output of rs href and aws vpc id; minor bug fixes.

--- bin/rs-create-vpc.sh
+++ bin/rs-create-vpc.sh
@@ -1,7 +1,7 @@
 #! /bin/sh -e
 
 # rs-create-vpc.sh <rs_cloud_id> <name> <cidr_block> <instance_tenancy> <description>
-# e.g. rs-create-vpc.sh 4 'Red Dwarf VPC' 10.1.1.1/24 default 'This is a test VPC.'
+# e.g. rs-create-vpc.sh 4 'Red Dwarf' "10.1.1.0/24" default 'This is a test VPC.'
 
 # RightScale (public) cloud IDs
 #1  US-East
@@ -27,21 +27,25 @@ cidr_block="$3"
 instance_tenanacy="$4"
 vpc_desc="$5"
 
-rs-login-dashboard.sh               # (run this first to ensure current session)
+#rs-login-dashboard.sh               # (run this first to ensure current session)
 
 url="https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/vpcs"
 echo "POST: $url"
 
-result=$(curl -v -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" -X PUT -d _=" " -d vpc[name]="$vpc_name" -d vpc[cidr_block]="$cidr_block" -d vpc[instance_tenancy]="$instance_tenancy" -d vpc[description]="$vpc_desc" "$url" 2>&1)
+api_result=$(curl -v -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" -X POST -d vpc[name]="$vpc_name" -d vpc[cidr_block]="$cidr_block" -d vpc[instance_tenancy]="$instance_tenancy" -d vpc[description]="$vpc_desc" "$url")
 
-case $result in
+case $api_result in
 	*"302 Found"*)
-		echo "VPC '$2' successfuly created."
+		echo "VPC '$vpc_name' successfuly created."
+		echo "$api_result" > /tmp/rs_api_examples.output.txt
+		rs-query-cloud.sh "$rs_cloud_id"    # (optional, to refresh dash index view of the region's VPCs)
+		vpc_rs_url="$(echo "$api_result" | awk '/Location:/ { print $3 }')"         # rs href
+		echo "$vpc_rs_url"
+	    curl -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" "$vpc_rs_url#tab_info_container" | grep vpc- | sed -e 's/.*<td>\(.*\)<\/td>.*/\1/p' | head -n 1       # aws vpc ID
 	;;
 	*)
-		echo "$result"
-		echo "Creation of VPC, $1 failed!"
+		echo "$api_result"
+		echo "Creation of VPC '$vpc_name' failed!"
+		exit 1
 	;;
 esac
-
-rs-query-cloud.sh "$rs_cloud_id"    # (optional, to refresh dash index view of the region's VPCs)
