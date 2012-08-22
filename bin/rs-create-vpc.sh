f8d04d22bd431768293f24af53b0ef2169849791
Chris Fordham <chris.fordham@rightscale.com>
2012-08-22 22:09:42 +1000

    Test new techniques for scraping.

--- bin/rs-create-vpc.sh
+++ bin/rs-create-vpc.sh
@@ -1,4 +1,4 @@
-#! /bin/sh -e
+#! /bin/bash
 
 # rs-create-vpc.sh <rs_cloud_id> <name> <cidr_block> <instance_tenancy> <description>
 # e.g. rs-create-vpc.sh 4 'Red Dwarf' "10.1.1.0/24" default 'This is a test VPC.'
@@ -12,7 +12,7 @@
 #6  Oregon
 #7  SA-São Paulo (Brazil)
 
-[[ ! $1 ]] && echo 'No RightScale cloud I provided.' && exit 1
+[[ ! $1 ]] && echo 'No RightScale cloud ID provided.' && exit 1
 [[ ! $2 ]] && echo 'No VPC name provided.' && exit 1
 [[ ! $3 ]] && echo 'No VPC CIDR block provided.' && exit 1
 [[ ! $4 ]] && echo 'No VPC instance tenancy provided.' && exit 1
@@ -27,25 +27,45 @@ cidr_block="$3"
 instance_tenanacy="$4"
 vpc_desc="$5"
 
-#rs-login-dashboard.sh               # (run this first to ensure current session)
+rs-login-dashboard.sh               # (run this first to ensure current session)
 
 url="https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/vpcs"
 echo "POST: $url"
 
-api_result=$(curl -v -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" -X POST -d vpc[name]="$vpc_name" -d vpc[cidr_block]="$cidr_block" -d vpc[instance_tenancy]="$instance_tenancy" -d vpc[description]="$vpc_desc" "$url")
+api_result=$(curl -v -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" -X POST -d vpc[name]="$vpc_name" -d vpc[cidr_block]="$cidr_block" -d vpc[instance_tenancy]="$instance_tenancy" -d vpc[description]="$vpc_desc" "$url" 2>&1)
+
+echo "$api_result" > /tmp/rs_api_examples.output.txt
+
+if grep flash_message <<< $api_result > /dev/null 2>&1; then
+     echo "$api_result" | grep flash_message | sed 's/<[^>]*>//g'
+     echo "Creation of VPC '$vpc_name' failed!"
+     exit 1
+fi
 
 case $api_result in
 	*"302 Found"*)
 		echo "VPC '$vpc_name' successfuly created."
-		echo "$api_result" > /tmp/rs_api_examples.output.txt
-		rs-query-cloud.sh "$rs_cloud_id"    # (optional, to refresh dash index view of the region's VPCs)
-		vpc_rs_url="$(echo "$api_result" | awk '/Location:/ { print $3 }')"         # rs href
-		echo "$vpc_rs_url"
-	    curl -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" "$vpc_rs_url#tab_info_container" | grep vpc- | sed -e 's/.*<td>\(.*\)<\/td>.*/\1/p' | head -n 1       # aws vpc ID
+		rs-query-cloud.sh "$rs_cloud_id"    # (optional, to refresh dash index view of the region's VPCs; just to be sure its updated)
+		vpc_rs_url="$(echo "$api_result" | awk '/Location:/ { print $3 }')"     # rs href
+		vpc_rs_url="${vpc_rs_url//[[:space:]]}"
+
+	    echo "$vpc_rs_url"
+
+        # by the vpc's view
+        #curl -s -b "$HOME/.rightscale/rs_dashboard_cookie.txt" \
+        #-H "X-Requested-With: XMLHttpRequest" \
+        #-H "Referer: https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/vpcs" \
+        #"$vpc_rs_url" | grep vpc- | sed 's/<[^>]*>//g' | sed -e 's/.*<td>\(.*\)<\/td>.*/\1/p' | head -n 1 | sed 's/  //g'  # aws vpc ID
+
+        # by index (inaccurate, vpc name must be unique)
+        curl -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" \
+        -H "X-Requested-With: XMLHttpRequest" \
+        -H "Referer: https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/vpcs" \
+        "https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/vpcs;vpcs" | grep "$vpc_name" -C 3 | grep vpc- | sed -e 's/.*<td>\(.*\)<\/td>.*/\1/p' | head -n 1 | sed 's/  //g'  # aws vpc ID
+
 	;;
 	*)
-		echo "$api_result"
-		echo "Creation of VPC '$vpc_name' failed!"
-		exit 1
+	    echo 'There appears to have been an issue creating the VPC, please check the result.'
+	    exit 255;
 	;;
 esac
