0a16108400871c58bf2b9140f0e5f87d6ab065f5
Chris Fordham <chris.fordham@rightscale.com>
2012-10-30 10:08:32 +1100

    Optional ebs_optimized option added (param 9).

--- bin/rs-create-server.sh
+++ bin/rs-create-server.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
-# rs-create-server.sh <nickname> <rs_cloud_id> <deployment_href> <server_template_href> <ec2_instance_type> <ec2_pricing> <ec2_ssh_key_href>
-# e.g. rs-create-server.sh 'Starbug' 3 "https://my.rightscale.com/api/acct/7954/deployments/49302" "https://my.rightscale.com/api/acct/7954/server_templates/262048001" 't1.micro' 'on_demand' "https://my.rightscale.com/acct/7954/clouds/3/ec2_ssh_keys/182526" 'us-west-1a' "https://my.rightscale.com/acct/7954/clouds/3/ec2_security_groups/200297001"
+# rs-create-server.sh <nickname> <rs_cloud_id> <deployment_href> <server_template_href> <ec2_instance_type> <ec2_pricing> <ec2_ssh_key_href> <ec2_security_group_hrefs> <e2_ebs_optimized>
+# e.g. rs-create-server.sh 'Starbug' 3 "https://my.rightscale.com/api/acct/7954/deployments/49302" "https://my.rightscale.com/api/acct/7954/server_templates/262048001" 't1.micro' 'on_demand' "https://my.rightscale.com/acct/7954/clouds/3/ec2_ssh_keys/182526" "https://my.rightscale.com/acct/7954/clouds/3/ec2_security_groups/106112" 1
 
 # RightScale (public) cloud IDs
 #1  US-East
@@ -19,8 +19,8 @@
 [[ ! $5 ]] && echo 'No EC2 instance type provided.' && exit 1
 [[ ! $6 ]] && echo 'No EC2 pricing type provided.' && exit 1
 [[ ! $7 ]] && echo 'No EC2 SSH key href provided.' && exit 1
-[[ ! $8 ]] && echo 'No EC2 availability zone provided.' && exit 1
-[[ ! $9 ]] && echo 'No EC2 security groups href provided.' && exit 1
+[[ ! $8 ]] && echo 'No EC2 security group provided.' && exit 1
+#[[ ! $9 ]] && echo 'No EC2 optmized option provided.' && exit 1
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
@@ -32,8 +32,9 @@ server_template_href="$4"
 ec2_instance_type="$5"
 ec2_pricing="$6"
 ec2_ssh_key_href="$7"
-ec2_availability_zone="$8"
-ec2_security_group_hrefs="$9"
+ec2_security_groups_href="$8"
+ec2_ebs_optimized="$9"
+: ${ec2_ebs_optimized:=0}
 
 url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers"
 echo "POST: $url"
@@ -47,12 +48,13 @@ result=$(curl -v -S -s -X POST \
 -d server[instance_type]="$ec2_instance_type" \
 -d server[pricing]="$ec2_pricing" \
 -d server[ec2_ssh_key_href]="$ec2_ssh_key_href" \
--d server[ec2_availability_zone]="$ec2_availability_zone" \
--d server[ec2_security_group_hrefs][]="$ec2_security_group_hrefs" \
+-d server[ec2_security_groups_href][]="$ec2_security_groups_href" \
+-d server[ebs_optimized]="$ec2_ebs_optimized" \
 -d cloud_id="$rs_cloud_id" \
 "$url" 2>&1)
 
 # other possible params
+#-d server[ec2_availability_zone]="$ec2_availability_zone"
 #-d server[aki_image_href]="$aki_image_href"
 #-d server[ari_image_href]="$ari_image_href"
 #-d server[ec2_image_href]="$ec2_image_href"
@@ -63,7 +65,7 @@ result=$(curl -v -S -s -X POST \
 #-d server[max_spot_price]="$ec2_max_spot_price"
 
 case $result in
-	*201*)
+	*'201 Created'*)
 		echo 'Server successfully created.'
 		echo "$result" | grep Location | awk '/Location:/ { print $3 }'
 		exit
