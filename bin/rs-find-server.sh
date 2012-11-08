0d335a7b8099f4cb8534d4cad87486146b109937
Chris Fordham <flaccid@lister.dev.xhost.net.au>
2012-11-08 13:13:57 +1100

    Initial add of rs-find-server.sh.

---
+++ bin/rs-find-server.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -e
+
+# rs-find-server.sh <server_nickname>
+
+[[ ! $1 ]] && echo 'No server nickname provided.' && exit 1
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+urlencode() {
+    local l=${#1}
+    for (( i = 0 ; i < l ; i++ )); do
+        local c=${1:i:1}
+        case "$c" in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            ' ') printf + ;;
+            *) printf '%%%X' "'$c"
+        esac
+    done
+}
+
+urldecode() {
+    local data=${1//+/ }
+    printf '%b' "${data//%/\x}"
+}
+
+nick_filter="$(urlencode "$1")"
+
+url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers?filter=nickname=$nick_filter"
+
+echo "GET: $url"
+
+result=$(curl -s -S -v -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
+
+echo "$result"
