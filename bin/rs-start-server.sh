ed913b9df65dbf52a225d2d52fff534a55f3bbf7
Chris Fordham <flaccid@lister.dev.xhost.net.au>
2012-11-09 10:46:13 +1100

    Initial add of rs-start-server.sh and rs-stop-server.sh.

---
+++ bin/rs-start-server.sh
@@ -0,0 +1,26 @@
+#! /bin/bash -e
+
+# rs-start-server.sh <server_id>
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+[[ ! $1 ]] && echo 'No server ID provided, exiting.' && exit 1
+server_id="$1"
+
+url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers/$server_id/start_ebs"
+
+echo "GET: $url"
+
+api_result=$(curl -s -S -v -X POST -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" $url 2>&1)
+
+case $api_result in
+    *"201 Created"*)
+      echo 'Server start request successfully sent.'
+    ;;
+    *)
+      echo 'Server start request failed!'
+      echo "$api_result"
+      exit 1
+    ;;
+esac
