1132b7420367bac33d42d558c9d4920c82b988c7
Chris Fordham <flaccid@lister.dev.xhost.net.au>
2012-11-19 11:10:56 +1100

    Fix minor syntax errors, add in logic to check result.

--- bin/rs-run-rightscript-array.sh
+++ bin/rs-run-rightscript-array.sh
@@ -14,11 +14,24 @@
 rightscript_id="$1"
 server_array_id="$2"
 server_template_id="$3"
-api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$server_array_id/run_script_on_all"
 
-echo "Running RightScript [$rightscript_id] on Server Array [$server_array_id]."
-echo "GET: $api_url"
-curl -H X-API-VERSION:"$rs_api_version" -X POST -b "$rs_api_cookie"
+url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$server_array_id/run_script_on_all"
+
+echo "Running RightScript [$rightscript_id] on Server Array [$server_array_id] from ServerTemplate [$server_template_id]."
+echo "POST: $url"
+
+api_result=$(curl -s -S -H X-API-VERSION:"$rs_api_version" -X POST -b "$rs_api_cookie" \
 -d "server_array[right_script_href]=https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts/$rightscript_id" \
 -d "server_array[server_template_hrefs]=https://my.rightscale.com/api/acct/$rs_api_account_id/ec2_server_templates/$server_template_id" \
-"$api_url"
+"$url" 2>&1)
+
+case "$api_result" in 
+  *audit-entries*)
+    echo "RightScript run successfully processed."
+    echo "$api_result"
+    exit
+  ;;
+  *)
+    echo "FAILED: $api_result"
+  ;;
+esac
