bc00ec3ef3f22154c4ace4de9261e1dfe2538f69
Chris Fordham <flaccid@lister.dev.xhost.net.au>
2012-11-19 12:33:38 +1100

    Add in support for RightScale API 1.5 for rs-run-rightscript-array.sh.

--- bin/rs-run-rightscript-array.sh
+++ bin/rs-run-rightscript-array.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
-# rs-run-rightscript-array.sh <rightscript_id> <server_array_id> <servertemplate_id>
+# rs-run-rightscript-array.sh <rightscript_id> <server_array_id> [<servertemplate_id>]
 
 # e.g.   rs-run-rightscript-array.sh 278706001 203474001 226619001
 
 [[ ! $1 ]] && echo 'No rightscript ID provided, exiting.' && exit 1
 [[ ! $2 ]] && echo 'No server array ID provided, exiting.' && exit 1
-[[ ! $3 ]] && echo 'No servertemplate ID provided, exiting.' && exit 1
+[[ ! $3 ]] && [ "$rs_api_version" != "1.5" ] && echo 'No servertemplate ID provided, exiting.' && exit 1
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
@@ -15,15 +15,22 @@ rightscript_id="$1"
 server_array_id="$2"
 server_template_id="$3"
 
-url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$server_array_id/run_script_on_all"
-
-echo "Running RightScript [$rightscript_id] on Server Array [$server_array_id] from ServerTemplate [$server_template_id]."
-echo "POST: $url"
-
-api_result=$(curl -s -S -H X-API-VERSION:"$rs_api_version" -X POST -b "$rs_api_cookie" \
+if [ "$rs_api_version" = "1.5" ]; then
+  url="https://my.rightscale.com/api/server_arrays/$server_array_id/multi_run_executable"
+  echo "Running RightScript [$rightscript_id] on Server Array [$server_array_id]."
+  echo "[API $rs_api_version] POST: $url"
+  api_result=$(curl -s -S -v -H X-API-VERSION:"$rs_api_version" -X POST -b "$rs_api_cookie" \
+-d "right_script_href=https://my.rightscale.com/api/right_scripts/$rightscript_id" \
+"$url" 2>&1)
+else
+  url="https://my.rightscale.com/api/acct/$rs_api_account_id/server_arrays/$server_array_id/run_script_on_all"
+  echo "Running RightScript [$rightscript_id] on Server Array [$server_array_id] from ServerTemplate [$server_template_id]."
+  echo "[API $rs_api_version] POST: $url"
+  api_result=$(curl -s -S -H X-API-VERSION:"$rs_api_version" -X POST -b "$rs_api_cookie" \
 -d "server_array[right_script_href]=https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts/$rightscript_id" \
 -d "server_array[server_template_hrefs]=https://my.rightscale.com/api/acct/$rs_api_account_id/ec2_server_templates/$server_template_id" \
 "$url" 2>&1)
+fi
 
 case "$api_result" in 
   *audit-entries*)
@@ -31,7 +38,12 @@ case "$api_result" in
     echo "$api_result"
     exit
   ;;
+  *202\ Accepted*)
+    echo "RightScript run successfully processed."
+    exit  
+  ;;
   *)
     echo "FAILED: $api_result"
+    exit 1
   ;;
 esac
