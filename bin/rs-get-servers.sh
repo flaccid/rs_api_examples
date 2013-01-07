39395e8f4e8b6b3e99d6ca17723712307ef45c57
Chris Fordham <chris@xhost.com.au>
2013-01-07 15:46:52 +1100

    Update rs-get-servers.sh with API 1.5 support.

--- bin/rs-get-servers.sh
+++ bin/rs-get-servers.sh
@@ -5,7 +5,24 @@
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
-url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers"
-echo "GET: $url"
-servers=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
-echo "$servers"
+case $rs_api_version in
+	*1.0*)
+    api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers"
+    echo "[API $rs_api_version] GET: $api_url"
+    api_result=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$api_url")
+	;;
+  *1.5*)
+    api_url="https://my.rightscale.com/api/servers.xml"
+    echo "[API $rs_api_version] GET: $api_url"
+    api_result=$(curl -s -S -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$api_url")
+  ;;
+esac
+
+case "$api_result" in 
+  \<*\>)
+    echo "$api_result"
+  ;;
+  *)
+    echo "FAILED: $api_result"
+  ;;
+esac
