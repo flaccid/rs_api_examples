bb6cded2fceafafdfa275517486c9097f91a7d89
Chris Fordham <chris@xhost.com.au>
2013-01-18 08:22:03 +1100

    Add basic filter support to rs-get-servers.sh.

--- bin/rs-get-servers.sh
+++ bin/rs-get-servers.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -e
 
-# rs-get-servers
+# rs-get-servers [<filter]
+
+# e.g. rs-get-servers.sh
+#      rs-get-servers.sh "nickname%3DUber+Servers%3A+Blitzkrieg"
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
+[ "$1" ] && filter="?filter%5B%5D=$1"
+
 case $rs_api_version in
 	*1.0*)
-    api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers"
+    api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/servers$filter"
     echo "[API $rs_api_version] GET: $api_url"
     api_result=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$api_url")
 	;;
