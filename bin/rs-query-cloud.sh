5ea6936ea268518fd16be3de4d0238ff18720a93
Chris Fordham <chris@xhost.com.au>
2013-04-09 09:07:11 +1000

    Add component/view param to rs-query-cloud.sh.

--- bin/rs-query-cloud.sh
+++ bin/rs-query-cloud.sh
@@ -3,7 +3,7 @@
 # Warning: This script posts information to the RightScale dashboard.
 # Do not use this script for production or any important purposes.  RightScale cannot guarantee this script to work now or in the future.
 
-# rs-query-cloud.sh <rs_cloud_id>
+# rs-query-cloud.sh <rs_cloud_id> <component>
 
 # RightScale (public) cloud IDs
 #1  US-East
@@ -14,14 +14,20 @@
 #6  Oregon
 #7  SA-São Paulo (Brazil)
 
+# Components/views
+# ec2_instances
+# ec2_ssh_keys
+
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
 [ ! "$1" ] && echo 'No RightScale cloud ID provided.' && exit 1
 
 rs_cloud_id="$1"
+component="$2"
+: ${component:=ec2_instances}
 
-api_url="https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/ec2_instances/requery"
+api_url="https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/$component/requery"
 echo "[dashboard] POST: $api_url"
 
 api_result=$(curl -X POST -v -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" -d "_method=put" --referer "https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/ec2_instances" "$api_url" 2>&1)
