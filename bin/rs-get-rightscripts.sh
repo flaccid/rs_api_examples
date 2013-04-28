983392de4601cbc94555f5b8af5e07cc424c36a4
Chris Fordham <chris.fordham@rightscale.com>
2013-04-28 12:24:16 +1000

    Add in extra presentational line break.

--- bin/rs-get-rightscripts.sh
+++ bin/rs-get-rightscripts.sh
@@ -11,6 +11,7 @@ rs_api_version=1.0
 
 api_url="https://my.rightscale.com/api/acct/$rs_api_account_id/right_scripts"
 echo "[API $rs_api_version] GET: $api_url"
+
 api_result=$(curl -s -H "X-API-VERSION: $rs_api_version" -b "$rs_api_cookie" "$api_url")
 
 echo "$api_result"
