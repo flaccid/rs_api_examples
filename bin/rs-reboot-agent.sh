dd37e96128f0fd9fa0641fb689c0947a98190962
Chris Fordham <chris@xhost.com.au>
2013-06-04 15:00:32 +1000

    Update stop rnac command to use ruby in sandbox.

--- bin/rs-reboot-agent.sh
+++ bin/rs-reboot-agent.sh
@@ -7,7 +7,7 @@
 echo 'Rebooting RightScale/RightLink.'
 
 # reboot rightscale/rightlink
-/opt/rightscale/right_link/scripts/rnac.rb --stop instance
+/opt/rightscale/sandbox/bin/ruby /opt/rightscale/right_link/bin/rnac.rb --stop instance
 service rightlink stop
 service rightscale start
 service rightboot start
