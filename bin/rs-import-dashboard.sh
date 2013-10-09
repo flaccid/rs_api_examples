88cfc24268143f651cb97e5fe931f941bc416059
Chris Fordham <chris@xhost.com.au>
2013-10-09 17:42:24 +1100

    Move rs-import.sh to rs-import-dashboard.sh for legacy support.

---
+++ bin/rs-import-dashboard.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+# rs-import.sh <library_id>
+
+# Warning: This script sends a request to the RightScale dashboard.
+# It is not recommended to use this script for production purposes. RightScale cannot guarantee this script will work now or in the future.
+
+[ ! "$1" ] && echo 'No library ID provided.' && exit 1
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+library_id="$1"
+
+url="https://my.rightscale.com/acct/$rs_api_account_id/library_helper/import/$library_id"
+echo "GET: $url"
+
+api_result=$(curl -s -H -S -I "X_API_VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
+
+echo "$api_result" | awk '/^Location:/ { print $2 }'
