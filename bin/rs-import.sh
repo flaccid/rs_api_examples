e8878c7bedbb693aa06996b4a9a9666ea2a7c3eb
Chris Fordham <chris@xhost.com.au>
2013-10-09 17:42:50 +1100

    Implement rs-import.sh with API 1.5.

--- bin/rs-import.sh
+++ bin/rs-import.sh
@@ -1,20 +1,32 @@
 #!/bin/sh -e
 
-# rs-import.sh <library_id>
+# rs-import.sh <publication_id>
 
-# Warning: This script sends a request to the RightScale dashboard.
-# It is not recommended to use this script for production purposes. RightScale cannot guarantee this script will work now or in the future.
+# To find the publication ID, open the publication in the dashboard from it's link in the marketplace, example:
+# opening http://www.rightscale.com/library/server_templates/Load-Balancer-with-HAProxy-1-5/lineage/45420
+# provides https://my.rightscale.com/library/server_templates/Load-Balancer-with-HAProxy-1-5/179414
+# => 179414 is the publication ID
 
-[ ! "$1" ] && echo 'No library ID provided.' && exit 1
+[ ! "$1" ] && echo 'No publication ID provided.' && exit 1
 
 . "$HOME/.rightscale/rs_api_config.sh"
 . "$HOME/.rightscale/rs_api_creds.sh"
 
-library_id="$1"
+# importing publications only supported in RS API 1.5
+rs_api_version='1.5'
 
-url="https://my.rightscale.com/acct/$rs_api_account_id/library_helper/import/$library_id"
-echo "GET: $url"
+publication_id="$1"
 
-api_result=$(curl -s -H -S -I "X_API_VERSION: $rs_api_version" -b "$rs_api_cookie" "$url")
+url="https://$rs_server/api/publications/$publication_id/import"; 
+echo "[API $rs_api_version] POST: $url"
 
-echo "$api_result" | awk '/^Location:/ { print $2 }'
+api_result=$(curl -S -s -v -X POST -H "X_API_VERSION: $rs_api_version" -b "$rs_api_cookie" -d account_href=/api/accounts/$rs_api_account_id "$url" 2>&1)
+
+case "$api_result" in 
+  *Status:\ 201*)
+    echo "$api_result" | awk '/^< Location:/ { print $3 }'
+  ;;
+  *)
+    echo "FAILED: $api_result"
+  ;;
+esac
