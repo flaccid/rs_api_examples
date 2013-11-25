4feea33242d4bee358467dc6cb1712bb7fef70dd
Chris Fordham <chris@xhost.com.au>
2013-11-26 09:02:09 +1100

    Fix unfinished stage with rs-query-cloud.sh; add in rs_server support.

--- bin/rs-query-cloud.sh
+++ bin/rs-query-cloud.sh
@@ -1,16 +1,9 @@
 #! /bin/sh -e
 
-<<<<<<< HEAD
 # Warning: This script posts information to the RightScale dashboard.
 # Do not use this script for production or any important purposes.  RightScale cannot guarantee this script to work now or in the future.
 
 # rs-query-cloud.sh <rs_cloud_id> <component>
-=======
-# rs-query-cloud.sh <rs_cloud_id>
->>>>>>> 8b61131c6279eee8d468250b2a268f3a3ee22a1a
-
-# Warning: This script posts information to the RightScale dashboard.
-# It is not recommended to use this script for production purposes. RightScale cannot guarantee this script will work now or in the future.
 
 # RightScale (public) cloud IDs
 #1  US-East
@@ -32,12 +25,12 @@
 
 rs_cloud_id="$1"
 component="$2"
-: ${component:=ec2_instances}
+: ${component:=instances}       # instances is UCP, ec2_instances is LCP
 
-api_url="https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/$component/requery"
+api_url="https://$rs_server/acct/$rs_api_account_id/clouds/$rs_cloud_id/$component/requery"
 echo "[dashboard] POST: $api_url"
 
-api_result=$(curl -X POST -v -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" -d "_method=put" --referer "https://my.rightscale.com/acct/$rs_api_account_id/clouds/$rs_cloud_id/ec2_instances" "$api_url" 2>&1)
+api_result=$(curl -X POST -v -s -S -b "$HOME/.rightscale/rs_dashboard_cookie.txt" -d "_method=put" --referer "https://$rs_server/acct/$rs_api_account_id/clouds/$rs_cloud_id/ec2_instances" "$api_url" 2>&1)
 
 # should equal referer
 echo "$api_result" | grep "Location: " | awk '{ print $3 }'
