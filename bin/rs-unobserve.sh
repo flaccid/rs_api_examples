31b2ce147f84969845d4cab5fb9d4283fe9e12c4
Chris Fordham <chris@xhost.com.au>
2014-01-28 12:57:35 +1100

    Add rs-[un]observe.sh scripts.

---
+++ bin/rs-unobserve.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+# Warning: This sends a request to the RightScale dashboard.
+# It is intended for internal purposes only.
+
+. "$HOME/.rightscale/rs_api_config.sh"
+. "$HOME/.rightscale/rs_api_creds.sh"
+
+rs_observe_cookie="$HOME/.rightscale/rs_observe_cookie.txt"
+
+curl -s -c "$rs_observe_cookie" -u "$rs_api_user":"$rs_api_password" \
+  "https://my.rightscale.com/api/acct/72/login?api_version=1.0"
+
+for account in "$@"
+do
+  echo "Attempting to un-observe account, $account..."
+  curl -b "$rs_api_cookie" \
+    -X POST \
+    -H "Referer:https://my.rightscale.com/global/admin_accounts/$account" \
+    -H "Host:my.rightscale.com" \
+  "https://my.rightscale.com/global/admin_accounts/$account/remove_access"
+done
