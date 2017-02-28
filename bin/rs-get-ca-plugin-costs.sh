7954855300572eff8e82f45fe747c51ed9973730
Chris Fordham <chris@fordham-nagy.id.au>
2017-03-01 10:05:32 +1100

    Use api access token if available.

--- bin/rs-get-ca-plugin-costs.sh
+++ bin/rs-get-ca-plugin-costs.sh
@@ -13,10 +13,17 @@ api_url='https://analytics.rightscale.com/api/plugin_costs'
 
 echo "[API $rs_ca_api_version] GET: $api_url"
 
-api_result=$(curl -Ss --include \
-  -H "X-API-Version: $rs_ca_api_version" \
-  -b "$rs_api_cookie"  \
-  --request GET "$api_url" 2>&1)
+if [ ! -z "$rs_api_access_token" ]; then
+  api_result=$(curl -Ss --include \
+    -H "X-API-Version: $rs_ca_api_version" \
+    -H "Authorization: Bearer $rs_api_access_token" \
+    --request GET "$api_url" 2>&1)
+else
+  api_result=$(curl -Ss --include \
+    -H "X-API-Version: $rs_ca_api_version" \
+    -b "$rs_api_cookie" \
+    --request GET "$api_url" 2>&1)
+fi
 
 case $api_result in
 	*'200 OK'*)
